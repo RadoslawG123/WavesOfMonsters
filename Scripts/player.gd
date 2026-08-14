8768f4fb9145a80ab1c6a8027e2b3fb358a031a0
Radosław Górecki <gorecki208@onet.pl>
2026-08-14 11:47:38 +0200

    Create player + basic player movement.

---
+++ Scripts/player.gd
@@ -0,0 +1,74 @@
+extends CharacterBody2D
+
+enum STATE {
+	FALL,
+	FLOOR,
+	JUMP,
+	FLOAT
+}
+const FALL_GRAVITY := 800.0
+const FALL_VELOCITY := 200.0
+const WALK_VELOCITY := 100.0
+const JUMP_VELOCITY := -400.0
+const JUMP_DECELERATION := 800.0
+
+@onready var player_animation: AnimatedSprite2D = %PlayerAnimation
+
+
+var active_state := STATE.FALL
+
+func _ready() -> void:
+	switch_state(active_state)
+
+func _physics_process(delta: float) -> void:
+	process_state(delta)
+	move_and_slide()
+
+func switch_state(to_state: STATE) -> void:
+	active_state = to_state
+
+	## State specific things that need to run only once upon entering the next state
+	match active_state:
+		STATE.FALL:
+			player_animation.play("Fall")
+			
+		STATE.JUMP:
+			player_animation.play("Jump")
+			velocity.y = JUMP_VELOCITY
+
+func process_state(delta: float) -> void:
+	match active_state:
+		STATE.FALL:
+			velocity.y = move_toward(velocity.y, FALL_VELOCITY, FALL_GRAVITY * delta)
+			handle_movement()
+			
+			if is_on_floor():
+				switch_state(STATE.FLOOR)
+			#elif Input.is_action_just_pressed("Jump"):
+				#switch_state(STATE.JUMP)
+			
+		STATE.FLOOR:
+			if Input.get_axis("Left", "Right"):
+				player_animation.play("Walk")
+			else:
+				player_animation.play("Idle")
+			handle_movement()
+			
+			if not is_on_floor():
+				switch_state(STATE.FALL)
+			elif Input.is_action_just_pressed("Jump"):
+				switch_state(STATE.JUMP)
+		
+		STATE.JUMP:
+			velocity.y = move_toward(velocity.y, 0, JUMP_DECELERATION * delta)
+			handle_movement()
+			
+			if Input.is_action_just_released("Jump") or velocity.y >= 0:
+				velocity.y = move_toward(velocity.y, 0, 10000.0 * delta)
+				switch_state(STATE.FALL)
+				
+func handle_movement() -> void:
+	var input_direction := signf(Input.get_axis("Left", "Right"))
+	if input_direction:
+		player_animation.flip_h = input_direction > 0
+	velocity.x = input_direction * WALK_VELOCITY
