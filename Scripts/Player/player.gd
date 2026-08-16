5eb4d19d34cf73195ba6c08641c9f69a83a517b5
Radosław Górecki <gorecki208@onet.pl>
2026-08-16 15:45:28 +0200

    Add Attack and Double Attack animation to Player.

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 enum STATE {
 	FALL,
 	FLOOR,
-	JUMP
+	JUMP,
 }
 const FALL_GRAVITY := 500.0
 const FALL_VELOCITY := 200.0
@@ -12,8 +12,11 @@ const JUMP_VELOCITY := -300.0
 const JUMP_DECELERATION := 700.0
 
 @onready var player_animation: AnimatedSprite2D = %PlayerAnimation
+@onready var first_attack_colldawn: Timer = $FirstAttackColldawn
 
-
+var can_attack := true
+var is_attacking := false
+var attack_combo := false
 var active_state := STATE.FALL
 
 func _ready() -> void:
@@ -27,6 +30,11 @@ func _physics_process(delta: float) -> void:
 	
 	## Adding infomration to DEBUG OVERLAY
 	DebugOverlay.add_stat("Player", "State:", active_state)
+	DebugOverlay.add_stat("Player", "Animation", player_animation.animation)
+	DebugOverlay.add_stat("Player", "attack_combo", attack_combo)
+	DebugOverlay.add_stat("Player", "is_attacking", is_attacking)
+	DebugOverlay.add_stat("Player", "can_attack", can_attack)
+	DebugOverlay.add_stat("Player", "AttackColldawn", first_attack_colldawn.time_left)
 	DebugOverlay.add_stat("Player", "Velocity X:", roundf(velocity.x))
 	DebugOverlay.add_stat("Player", "Velocity Y:", roundf(velocity.y))
 
@@ -41,7 +49,7 @@ func switch_state(to_state: STATE) -> void:
 		STATE.JUMP:
 			player_animation.play("Jump")
 			velocity.y = JUMP_VELOCITY
-
+			
 func process_state(delta: float) -> void:
 	match active_state:
 		STATE.FALL:
@@ -54,15 +62,31 @@ func process_state(delta: float) -> void:
 				#switch_state(STATE.JUMP)
 			
 		STATE.FLOOR:
-			if Input.get_axis("Left", "Right"):
-				player_animation.play("Walk")
-			else:
-				player_animation.play("Idle")
+			if not is_attacking:
+				if Input.get_axis("Left", "Right"):
+					player_animation.play("Walk")
+				else:
+					player_animation.play("Idle")
+			elif not attack_combo:
+				player_animation.play("Attack")
+			
+			if Input.is_action_just_pressed("Attack"):
+				if can_attack and not attack_combo:
+					first_attack_colldawn.start()
+					is_attacking = true
+				elif is_attacking:
+					attack_combo = true
+					
+				if not attack_combo:
+					can_attack = false
+			
 			handle_movement()
 			
 			if not is_on_floor():
+				reset_attacks()
 				switch_state(STATE.FALL)
 			elif Input.is_action_just_pressed("Jump"):
+				reset_attacks()
 				switch_state(STATE.JUMP)
 		
 		STATE.JUMP:
@@ -82,6 +106,23 @@ func handle_movement() -> void:
 		player_animation.flip_h = input_direction > 0
 	velocity.x = input_direction * WALK_VELOCITY
 
+func reset_attacks():
+	is_attacking = false
+	attack_combo = false
+
 func _on_animation_finished():
 	if player_animation.animation == "Jump":
 		player_animation.play("Float")
+		
+	elif player_animation.animation == "Attack":
+		if attack_combo:
+			player_animation.play("Double_Attack")
+		else:
+			reset_attacks()
+			
+	elif player_animation.animation == "Double_Attack":
+		reset_attacks()
+
+
+func _on_first_attack_colldawn_timeout() -> void:
+	can_attack = true
