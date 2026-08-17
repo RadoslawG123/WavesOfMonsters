80300d11c5d9af19c764b9c66999acca96d1fba0
Radosław Górecki <gorecki208@onet.pl>
2026-08-17 21:52:39 +0200

    SpearGoblin, Hurtbox Component, Hitbox Component, Health Component, working attack mechanics: in progress.

---
+++ Scripts/Universal/HealthComponent.gd
@@ -0,0 +1,10 @@
+extends Node
+class_name HealthComponent
+
+@export var health_amount := 1
+
+func add_health(health: int) -> void:
+	health_amount = health
+
+func received_damage() -> void:
+	health_amount -= 1
---
+++ Scripts/Universal/Hitbox.gd
@@ -0,0 +1,14 @@
+extends Node
+class_name Hitbox
+
+@onready var hitbox_1: Area2D = %Hitbox1
+@onready var hitbox_2: Area2D = %Hitbox2
+
+## Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#pass # Replace with function body.
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
